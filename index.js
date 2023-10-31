e9567405037459d8c074962649a2863d60d96713
Andre Schnyder Castello Branco <andrecb18@yahoo.com.br>
2023-10-31 11:34:23 -0700

    Solutions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (argument = 'go to the office') {
+    return `This Monday, I will ${argument}.`
+}
+
+function wrapAdjective (flair = '*') {
+    return function (string) {
+        return`You are ${flair}${string}${flair}!`
+    }
+}
+
